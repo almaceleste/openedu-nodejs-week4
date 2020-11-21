e6143e0776423a8bcc0121620b459bffa7622cdb
almaceleste <pcaptanovska@gmail.com>
2020-11-21 06:30:43 +0300

    upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test',
+};
+
+const s = require('http').Server((req,res) => {
+    let body = '';
+    switch (req.url) {
+        case '/result4/':
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                res.writeHead(200, {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    ...CORS
+                });
+                res.write(JSON.stringify({
+                    message: 'hessian',
+                    'x-result': req.headers['x-test'],
+                    'x-body': body
+                }));
+                return res.end('\n');
+            });
+            break;
+        default:
+            res.writeHead(200, {
+                ...CORS
+            });
+            res.write('¡hola, mundo!');
+            return res.end('\n');
+            break;
+    }
+    // res.end('\n');
+});
+s.listen(4321);
